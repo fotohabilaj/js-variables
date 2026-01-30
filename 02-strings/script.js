88319b12dfcec143ecfb2c89b8771d920ed42c54
fotohabilaj <fotohabilaj@gmail.com>
2026-01-30 12:56:31 +0100

    completato 0202

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -8,6 +8,15 @@ console.log(namee);
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+let number= '32';
+let text= 'ak';
+let bool= '0'
+console.log(number);
+console.log(text);
+console.log(bool);
+
+
+
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
